796167050233803324e21b3fb2df11407d69c3be
Morgan Kenyon <morganwkenyon@gmail.com>
2022-03-02 20:16:06 -0600

    Adding unit test for failure to parse

--- src/Library.fs
+++ src/Library.fs
@@ -16,10 +16,10 @@ module TimeDuration =
     let phour = pint32 .>> (pchar 'h' <|> pchar 'H') |>> fun x -> Hour(x)
 
     
-    let parse value =
+    let parse (value: string): Result<Interval, string> =
         let result = run phour value
         match result with 
         | Success (u, _, _) -> 
             match u with 
-            | Hour x -> Some { Hours = x }
-        | Failure (_, _, _) -> None
+            | Hour x -> Microsoft.FSharp.Core.Result<_,_>.Ok { Hours = x }
+        | Failure (m, _, _) -> Microsoft.FSharp.Core.Result<_,_>.Error m
--- test/TimeDurationTests.fs
+++ test/TimeDurationTests.fs
@@ -9,10 +9,28 @@ let shouldBeSome value =
     | Some x -> x
     | None -> failwith "cannot be none"
 
+let shouldBeOkay value = 
+    match value with
+    | Ok r -> r
+    | Error _ -> failwith "cannot be error"
+
+let shouldBeError value = 
+    match value with
+    | Ok _ -> failwith "cannot be ok"
+    | Error e -> e
+
 [<Fact>]
 let ``My test`` () =
     let value = "8h"
     
-    let result = parse value |> shouldBeSome
+    let result = parse value |> shouldBeOkay
 
     Assert.Equal(8, result.Hours)
+
+[<Fact>]
+let ``can return none when nothing to parse`` () =
+    let value = "8"
+
+    let result = parse value |> shouldBeError
+
+    Assert.Equal("Error in Ln: 1 Col: 2\r\n8\r\n ^\r\nNote: The error occurred at the end of the input stream.\r\nExpecting: 'H' or 'h'\r\n", result)
