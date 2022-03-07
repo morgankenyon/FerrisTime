6cf98f4bae42c1a34037b0a74875a1564792fa53
Morgan Kenyon <morganwkenyon@gmail.com>
2022-03-06 20:34:58 -0600

    adding single parsing of minute, day, and hour

--- src/Library.fs
+++ src/Library.fs
@@ -6,20 +6,27 @@ module Say =
 
 module TimeDuration =
     open FParsec
-    type Interval = { Hours: int }
+    type Interval = { Days: int; Hours: int; Minutes: int }
 
     type Unit = 
+    | Day of int
     | Hour of int
-    
+    | Minute of int
 
-    let str s = pstring s
+    let pday = pint32 .>> (pchar 'd' <|> pchar 'D') |>> fun x -> Day(x)
     let phour = pint32 .>> (pchar 'h' <|> pchar 'H') |>> fun x -> Hour(x)
-
+    let pminute = pint32 .>> (pchar 'm' <|> pchar 'M') |>> fun x -> Minute(x)
+    let attemptDay = attempt pday
+    let attemptHour = attempt phour
+    let attemptMinute = attempt pminute
+    let pduration = attemptDay <|> attemptHour <|> attemptMinute
     
     let parse (value: string): Result<Interval, string> =
-        let result = run phour value
+        let result = run pduration value
         match result with 
         | Success (u, _, _) -> 
             match u with 
-            | Hour x -> Microsoft.FSharp.Core.Result<_,_>.Ok { Hours = x }
-        | Failure (m, _, _) -> Microsoft.FSharp.Core.Result<_,_>.Error m
+            | Day d -> Core.Result<_,_>.Ok { Days = d; Hours = 0; Minutes = 0 }
+            | Hour h -> Core.Result<_,_>.Ok { Days = 0; Hours = h; Minutes = 0 }
+            | Minute m -> Core.Result<_,_>.Ok { Days = 0; Hours = 0; Minutes = m }
+        | Failure (_, _, _) -> Core.Result<_,_>.Error $"'{value}' cannot be parsed"
--- test/TimeDurationTests.fs
+++ test/TimeDurationTests.fs
@@ -12,25 +12,48 @@ let shouldBeSome value =
 let shouldBeOkay value = 
     match value with
     | Ok r -> r
-    | Error _ -> failwith "cannot be error"
+    | Error _ -> failwith $"cannot be error"
 
 let shouldBeError value = 
     match value with
     | Ok _ -> failwith "cannot be ok"
     | Error e -> e
 
+let doesResultEqual result d h m =
+    Assert.Equal(d, result.Days)
+    Assert.Equal(h, result.Hours)
+    Assert.Equal(m, result.Minutes)
+
+
 [<Fact>]
-let ``My test`` () =
+let ``can parse single minute`` () =
+    let value = "8m"
+    
+    let result = parse value |> shouldBeOkay
+
+    doesResultEqual result 0 0 8
+
+[<Fact>]
+let ``can parse single hour`` () =
     let value = "8h"
     
     let result = parse value |> shouldBeOkay
 
-    Assert.Equal(8, result.Hours)
+    doesResultEqual result 0 8 0
 
 [<Fact>]
-let ``can return none when nothing to parse`` () =
+let ``can parse single day`` () =
+    let value = "8d"
+    
+    let result = parse value |> shouldBeOkay
+
+    doesResultEqual result 8 0 0
+
+[<Fact>]
+let ``can return error message when nothing to parse`` () =
     let value = "8"
 
     let result = parse value |> shouldBeError
 
-    Assert.Equal("Error in Ln: 1 Col: 2\r\n8\r\n ^\r\nNote: The error occurred at the end of the input stream.\r\nExpecting: 'H' or 'h'\r\n", result)
+    //Assert.Equal("Error in Ln: 1 Col: 2\r\n8\r\n ^\r\nNote: The error occurred at the end of the input stream.\r\nExpecting: 'H' or 'h'\r\n", result)
+    Assert.Equal("'8' cannot be parsed", result)
